78de84f170827f2c574d9f66877486b7ff8b1a36
Ludwig <ludwig.stumpp@unternehmertum.de>
2021-10-21 16:08:32 +0200

    Template for the init function

--- helpers/cellular_automaton.py
+++ helpers/cellular_automaton.py
@@ -1,2 +1,14 @@
+from typing import Optional
+
+
 class CellularAutomaton():
-    pass
+    def __init__(self, config_file: Optional[str] = None, grid_size: Optional[tuple[int, int]] = None):
+        if config_file is None and grid_size is None:
+            raise ValueError('Either need "config_file" or "grid_size" to be specified.')
+
+        if config_file is not None:
+            # parse config file
+            pass
+        else:
+            # create empty grid according to grid_size
+            pass
